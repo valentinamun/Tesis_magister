cbbedcda951416760b14524299a2eb780a5c8899
valentinamun <85357786+valentinamun@users.noreply.github.com>
2022-09-14 13:24:04 -0300

    Update clim_mosa.sh

--- clim_mosa.sh
+++ clim_mosa.sh
@@ -2,7 +2,7 @@
 
 #Concatenar variables separadas por semestres 
 
-for x in {4..9}
+for x in {4..8}
 
 do
 
@@ -10,9 +10,9 @@ ncrcat Odette_fisica_Y${x}S1.nc  Odette_fisica_Y${x}S2.nc mosa_0${x}.nc
 
 done
 
-#Concatenar en una sola variable del año 4 al año 9 de simulacion
+#Concatenar en una sola variable del año 4 al año 8 de simulacion
 
-ncrcat mosa_0?.nc mosa_4-9.nc
+ncrcat mosa_0?.nc mosa_4-8.nc
 
 
 #Climatologia
@@ -20,15 +20,15 @@ ncrcat mosa_0?.nc mosa_4-9.nc
 for k in {1..9}
 do
 
-ncra -F -d time,${k},72,12 mosa_4-9.nc clim_4-9_0${k}.nc
+ncra -F -d time,${k},60,12 mosa_4-8.nc clim_4-8_0${k}.nc
 
 done
 
 for k in {10..12}
 do
 
-ncra -F -d time,${k},72,12 mosa_4-9.nc clim_4-9_${k}.nc
+ncra -F -d time,${k},60,12 mosa_4-8.nc clim_4-8_${k}.nc
 
 done
 
-ncrcat clim_4-9_??.nc clim_fisic_mosa_4-9.nc
+ncrcat clim_4-8_??.nc clim_fisic_mosa_4-8.nc
