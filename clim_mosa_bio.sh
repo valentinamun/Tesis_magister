0558d2d95c15f59520bb0debf2cfe843cf62df61
valentinamun <85357786+valentinamun@users.noreply.github.com>
2022-09-14 13:40:07 -0300

    Add files via upload

---
+++ clim_mosa_bio.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Concatenar variables separadas por semestres 
+
+for x in {4..8}
+
+do
+
+ncrcat Odette_Y${x}S1.nc  Odette_Y${x}S2.nc mosa_0${x}.nc
+
+done
+
+#Concatenar en una sola variable del año 4 al año 8 de simulacion
+
+ncrcat mosa_0?.nc mosa_4-8.nc
+
+
+#Climatologia
+
+for k in {1..9}
+do
+
+ncra -F -d time,${k},60,12 mosa_4-8.nc clim_4-8_0${k}.nc
+
+done
+
+for k in {10..12}
+do
+
+ncra -F -d time,${k},60,12 mosa_4-8.nc clim_4-8_${k}.nc
+
+done
+
+ncrcat clim_4-8_??.nc clim_bio_mosa_4-8.nc
