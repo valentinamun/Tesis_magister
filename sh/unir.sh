f73b6a762c8dcfbfa8302552d2f8e750603dc773
valentinamun <85357786+valentinamun@users.noreply.github.com>
2023-03-07 17:27:58 -0300

    Add files via upload

---
+++ sh/unir.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ncks -A Mercator_W_M1_2022.nc Mercator_U_V_M1_2022.nc
+ncks -A Mercator_W_M2_2022.nc Mercator_U_V_M2_2022.nc
+
+
+#ncrcat mosa_BGQ_avg_VM_Y1_M12_HM.nc  mosa_BGQ_avg_VM_Y2_M1_HM.nc prueba.nc
+
