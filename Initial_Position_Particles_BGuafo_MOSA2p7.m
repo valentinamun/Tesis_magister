9f04b971352b671fede8d65e1816cb8102cf7e06
Andres Sepulveda <AndresSepulveda@users.noreply.github.com>
2023-03-08 11:09:26 -0300

    Create Initial_Position_Particles_BGuafo_MOSA2p7.m

---
+++ Initial_Position_Particles_BGuafo_MOSA2p7.m
@@ -0,0 +1,18 @@
+start
+nc=netcdf('back_opendrift.nc','r');
+status=nc{'status'}(:,1);
+lon=nc{'lon'}(:,1);
+lat=nc{'lat'}(:,1);
+z=nc{'z'}(:,1);
+close(nc)
+
+indx=find(status < 1);      % 0 :active, 1 : seafloor
+g_lon=lon(indx);
+g_lat=lat(indx);
+g_z=z(indx);
+
+plot(g_lat,g_z,'x')
+
+%
+% 4893 of 1000
+%
